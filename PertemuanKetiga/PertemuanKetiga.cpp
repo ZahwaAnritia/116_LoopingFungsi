1d521d56f668cc3211600492ea23f3ff5fc56149
ZahwaAnritia <anritia15@gmail.com>
2024-03-18 11:44:47 +0700

    Add project files.

---
+++ PertemuanKetiga/PertemuanKetiga.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+
+    for (i = 0; i < 6; i++) {
+        cout << "Nama saya adalah Zahwa" << endl;
+
+    }
+
+    string arr[5];
+
+    for (i = 0; i < 5; i++) {
+        cout << "Masukkan nickname ke " << i + 1 << " = ";
+        cin >> arr[i];
+
+
+    }
+    for (i = 0; i < 5; i++) {
+        cout << "Namanya adalah = " << arr[i] << endl;
+    }
+
+    return 0;
+}
---
+++ PertemuanKetiga1/PertemuanKetiga1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	int x;
+	srand(time(0));
+
+	x = rand() % 10;
+
+	cout << "Nilai X Awal = " << x << endl;
+
+	while (x > 4)
+	{
+		cout << "Perulangan Dengan While" << endl;
+		x = rand() % 10;
+	}
+	
+	cout << "Nilai X akhir = " << x << endl;
+
+	return 0;
+
+}
